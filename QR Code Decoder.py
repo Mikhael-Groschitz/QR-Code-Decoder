58244082afe42fab847ccfdd3fddd710757ddc77
Mika Hell <161988867+Mikhael-Groschitz@users.noreply.github.com>
2024-03-28 10:36:42 -0300

    QR Code Decoder.py
    Changing the code to have a better understanding of how to use

--- QR Code Decoder.py
+++ QR Code Decoder.py
@@ -1,4 +1,4 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
-decocdeQR = decode(Image.open("github.png"))
+decocdeQR = decode(Image.open("insert image name.png")
 print(decocdeQR[0].data.decode('ascii'))
